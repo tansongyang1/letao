38b9cbc8a483288137afa96f63b8b163db9c7220
tansongyang1 <1017772475@qq.com>
2019-03-21 14:16:02 +0800

    用户页面修改完成

--- public/admin/js/comment.js
+++ public/admin/js/comment.js
@@ -46,23 +46,23 @@ $(function () {
     })
   })
 
-  // 封装分页
-  function page(info,render) {
-    $('#paginator').bootstrapPaginator({
-      // 版本号
-      bootstrapMajorVersion: 3,
-      // 当前页
-      currentPage: info.page,
-      // 总页数
-      totalPages: Math.ceil(info.total / info.size),
-
-      // 给页码添加点击事件
-      onPageClicked: function (a, b, c, page) {
-        // 将选中的页码更新到 currentPage
-        currentPage = page;
-        // 重新渲染
-        render();
-      }
-    })
-  }
 })
+// 封装分页
+    function paginator(info,render) {
+      $('#paginator').bootstrapPaginator({
+        // 版本号
+        bootstrapMajorVersion: 3,
+        // 当前页
+        currentPage: info.page,
+        // 总页数
+        totalPages: Math.ceil(info.total / info.size),
+  
+        // 给页码添加点击事件
+        onPageClicked: function (a, b, c, page) {
+          // 将选中的页码更新到 currentPage
+          // currentPage = page;
+          // 重新渲染
+          render(page);
+        }
+      })
+    }
--- public/admin/js/user.js
+++ public/admin/js/user.js
@@ -1,14 +1,13 @@
 $(function () {
-  var currentPage = 1;
   var pageSize = 5;
-  render();
+  render(1);
   // 渲染
-  function render() {
+  function render(page) {
     $.ajax({
       url: '/user/queryUser',
       type: 'get',
       data: {
-        page: currentPage,
+        page: page,
         pageSize: pageSize
       },
       // dataType: 'json',
@@ -17,24 +16,25 @@ $(function () {
 
         var tmp = template("tmp", info);
         $('tbody').html(tmp);
-        // page();
-        // 分页初始化
-        $('#paginator').bootstrapPaginator({
-          // 版本号
-          bootstrapMajorVersion: 3,
-          // 当前页
-          currentPage: info.page,
-          // 总页数
-          totalPages: Math.ceil(info.total / info.size),
+        paginator(info,render);
+        
+        // // 分页初始化
+        // $('#paginator').bootstrapPaginator({
+        //   // 版本号
+        //   bootstrapMajorVersion: 3,
+        //   // 当前页
+        //   currentPage: info.page,
+        //   // 总页数
+        //   totalPages: Math.ceil(info.total / info.size),
 
-          // 给页码添加点击事件
-          onPageClicked: function (a, b, c, page) {
-            // 将选中的页码更新到 currentPage
-            currentPage = page;
-            // 重新渲染
-            render();
-          }
-        })
+        //   // 给页码添加点击事件
+        //   onPageClicked: function (a, b, c, page) {
+        //     // 将选中的页码更新到 currentPage
+        //     currentPage = page;
+        //     // 重新渲染
+        //     render();
+        //   }
+        // })
       }
 
     })
